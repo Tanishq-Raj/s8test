41aacbafe88cdc9efbec66e4a176ac8bd55ebdfb
Tanishq-Raj <135883722+Tanishq-Raj@users.noreply.github.com>
2025-02-27 12:14:20 +0530

    Update sendToken.js

--- server/src/utils/sendToken.js
+++ server/src/utils/sendToken.js
@@ -5,8 +5,8 @@ export const sendToken = (user, message, res) => {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ), 
-      sameSite: 'none', 
-      secure: true
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: process.env.NODE_ENV === 'production'
     })
     .json({
       success: true,
